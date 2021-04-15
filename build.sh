0f936d61b83428c742ab471a7bcae49b06daea7b
alexwcrafter <39865767+alexwcrafter@users.noreply.github.com>
2021-04-15 20:24:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,7 +23,6 @@ elif [ "${ccache}" == "true" ] && [ -z "${ccache_size}" ]; then
     echo "Please set the ccache_size variable in your config."
     exit 1
 fi
-build/soong/soong_ui.bash --make-mode
 lunch "${rom_vendor_name}_${device}-${buildtype}"
 rm -f "${outdir}"/*2021*.zip
 rm -f "${outdir}"/*2021*.zip.md5
