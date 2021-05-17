5a79a16f5b929a66fc88f42832d87ef0ae3b15f5
alexwcrafter <39865767+alexwcrafter@users.noreply.github.com>
2021-05-17 19:52:46 +0200

    Update config.backup.sh

--- config.backup.sh
+++ config.backup.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
 
+export GITHUB_USER="alexwcrafter"
+export GITHUB_EMAIL="wesjalex@gmail.com"
+
+export device="sofiar"
+
+export ROM="dotos"
+export ROM_DIR="${WORKSPACE}/rom"
+export ROM_VERSION="11.0"
+export official="false"
+export local_manifest_url="https://github.com/alexwcrafter/local_manifests/raw/dot11/sofiar.xml"
+export manifest_url="https://github.com/DotOS/manifest"
+export rom_vendor_name="dot"
+export branch="dot11"
+export bacon="bacon"
+export buildtype="installclean"
+export clean="clean"
+export upload_recovery="false"
+
+export ccache="false"
+export ccache_size=""
+
+export jenkins="true"
+
+export release_repo="alexwcrafter/releases"
+
+export timezone="UTC"
